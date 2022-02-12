cd981f5c63fef4a4301714e82f720ffbcc915621
anmolgrwl17 <anmolgrwl17@gmail.com>
2022-02-13 00:32:17 +0530

    auctionFEBE Connect

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 import "../../App.css";
 import Cards from "../Cards";
 import HeroSection from "../HeroSection";
+import HomeTest from "./homeTest";
 import Footer from "../Footer";
+
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <HomeTest />
       <Footer />
     </>
   );
